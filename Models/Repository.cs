e2ac56ffc13e91cdadfebeb4281494814390ef58
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-08 00:51:15 +0300

    Created repository model and added some test datas and necessary methods.

---
+++ Models/Repository.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieApp.Models
+{
+    public static class Repository
+    {
+        private static List<Movie> _movies = null;
+
+        static Repository()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie(){ Id=1, Name = "The Lighthouse", Description="Two lighthouse keepers try to maintain their sanity whilst living on a remote and mysterious New England island in the 1890s.", ImageUrl="lighthouse.jpg"},
+                new Movie(){ Id=2, Name = "Parasite", Description="A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.", ImageUrl="parasite.jpg"},
+                new Movie(){ Id=3, Name = "Suspiria", Description="A darkness swirls at the center of a world-renowned dance company, one that will engulf the artistic director, an ambitious young dancer, and a grieving psychotherapist. Some will succumb to the nightmare. Others will finally wake up.", ImageUrl="Suspiria.jpg"},
+                new Movie(){ Id=4, Name = "2001: A Space Odyssey", Description="After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.", ImageUrl="a_space_odyssey.jpg"},
+                new Movie(){ Id=5, Name = "Spider-Man: Far from Home", Description="Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.", ImageUrl="Spider-Man_Far from Home.jpg"}
+            };
+        }
+
+        public static List<Movie> Movies{ get { return _movies; } }
+
+        public static void AddMovie(Movie entity){
+            _movies.Add(entity);
+        }
+
+        public static Movie GetById(int id){
+            return _movies.FirstOrDefault(i => i.Id == id);
+        }
+    }
+}
