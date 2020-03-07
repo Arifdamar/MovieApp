ae348091a8303a9c6a9d890de5d4eec3c3448d40
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-08 01:25:28 +0300

    Added Bootstrap backed views.

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -15,7 +15,7 @@ namespace MovieApp.Models
                 new Movie(){ Id=2, Name = "Parasite", Description="A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.", ImageUrl="parasite.jpg"},
                 new Movie(){ Id=3, Name = "Suspiria", Description="A darkness swirls at the center of a world-renowned dance company, one that will engulf the artistic director, an ambitious young dancer, and a grieving psychotherapist. Some will succumb to the nightmare. Others will finally wake up.", ImageUrl="Suspiria.jpg"},
                 new Movie(){ Id=4, Name = "2001: A Space Odyssey", Description="After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.", ImageUrl="a_space_odyssey.jpg"},
-                new Movie(){ Id=5, Name = "Spider-Man: Far from Home", Description="Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.", ImageUrl="Spider-Man_Far from Home.jpg"}
+                new Movie(){ Id=5, Name = "Spider-Man: Far from Home", Description="Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.", ImageUrl="Spider-Man_Far_from_Home.jpg"}
             };
         }
 
